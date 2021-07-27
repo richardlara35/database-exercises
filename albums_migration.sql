45b71c318016632a5ac3d1b5d475cc6880fb730d
richardlara35 <richard.u.lara35@gmail.com>
2021-07-27 16:04:19 -0500

    exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,5 +1,13 @@
-USE codeup_test_db;
-DROP TABLE IF EXISTS;
+USE
+codeup_test_db;
+DROP TABLE IF EXISTS albums;
 CREATE TABLE albums
-(id INT UNSIGNED NOT NULL AUTO_INCREMENT,artist VARCHAR(100), name VARCHAR(200),
-release_date DATE, sales float, genre VARCHAR(50), PRIMARY KEY(id));
+(
+    id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist       VARCHAR(100),
+    name         VARCHAR(200),
+    release_date INT,
+    sales        float,
+    genre        VARCHAR(50),
+    PRIMARY KEY (id)
+);
---
+++ select_exercises.sql
@@ -0,0 +1,7 @@
+SELECT * from albums
+where name = 'Sgt. Peppers Lonely Hearts Club Band';
+
+# SELECT name, artist FROM albums
+# WHERE artist = 'Pink Floyd';
+
+
