b776f8df5c33de3ccc0b214fcac9eddedeb7a2d7
richardlara35 <richard.u.lara35@gmail.com>
2021-07-27 16:28:44 -0500

    exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,7 +1,19 @@
-SELECT * from albums
+SELECT release_date from albums
 where name = 'Sgt. Peppers Lonely Hearts Club Band';
 
-# SELECT name, artist FROM albums
-# WHERE artist = 'Pink Floyd';
+SELECT name, artist FROM albums
+WHERE artist = 'Pink Floyd';
+
+SELECT genre from albums
+where name = 'Nevermind';
+
+select name, release_date from albums
+where release_date BETWEEN 1990 AND 2000;
+
+SELECT name, sales from albums
+where sales < 20.0;
+
+SELECT name, genre FROM albums
+WHERE genre = 'rock';
 
 
